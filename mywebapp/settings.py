82051794e5730aedd4f89e5bd48bfb87019711f1
tact01 <christbleighton@gmail.com>
2024-03-10 16:08:41 +0300

    added changes to settings file again.

--- mywebapp/settings.py
+++ mywebapp/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_ = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
