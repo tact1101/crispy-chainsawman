733c1978f4ad00af24742ec117a8e849bd32b53c
tact01 <christbleighton@gmail.com>
2024-03-10 17:06:34 +0300

    idk.

--- mywebapp/settings.py
+++ mywebapp/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/media/'
+MEDIA_ = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
